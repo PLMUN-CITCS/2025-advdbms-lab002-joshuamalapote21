3201437d6e65d7c3ef734512d2b178fe00b5b851
joshuamalapote21 <joshuamalapote21@gmail.com>
2025-03-26 17:26:01 +0800

    Completed assignment: Created tables and inserted data

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -1,12 +1,6 @@
-/*
-    Filename:       01_create_tables.sql
-    Purpose:        Create tables for the database
-    Instructions:
-    1. Create a table named tblStudents with the following columns:
-        - student_id (INT, PRIMARY KEY, AUTO_INCREMENT)
-        - student_name (VARCHAR(50))
-        - student_email (VARCHAR(50))
-        - student_phone (VARCHAR(50))
-
-    2. Place your create table query below this comment
-*/
+CREATE TABLE tblStudents (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(50),
+    student_email VARCHAR(50),
+    student_phone VARCHAR(50)
+);
